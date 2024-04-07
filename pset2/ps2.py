d39d3f1e074e1a7fbeac6f677bcc787be96291d3
Yaohui Wu <108550452+yaohui-wu@users.noreply.github.com>
2024-04-07 19:31:36 -0400

    Update Problem Set 2

--- pset2/ps2.py
+++ pset2/ps2.py
@@ -359,9 +359,15 @@ def showPlot2(title, x_label, y_label):
 #       (... your call here ...)
 #
 
+showPlot1('Time It Takes 1 - 10 Robots To Clean 80% Of A Room',
+          'Number of Robots', 'Time-steps')
+
 #
 # 2) Write a function call to showPlot2 that generates an appropriately-labeled
 #     plot.
 #
 #       (... your call here ...)
 #
+
+showPlot2('Time It Takes Two Robots To Clean 80% Of Variously Shaped Rooms',
+          'Aspect Ratio', 'Time-steps')
